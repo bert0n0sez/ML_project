f1bc162039154e255feb41a23e7e8af0f658a8c2
Albert <a.iktisanov@yandex.ru>
2023-12-06 02:02:39 +0300

    2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+from classes_oop import Primary_Data_Processing, Machine_Learning, Scores
+
+
+def main():
+    primary = Primary_Data_Processing()
+    machine = Machine_Learning()
+    score = Scores()
+    data = primary.None_Processing() 
+    type = machine.lin_svm(data)
+    final_score = score.f1_macro(type)
+    print('Наконец-то у меня спустя 5 часов дебага получился ответ:', final_score)
+
+if __name__ == '__main__':
+    main()
