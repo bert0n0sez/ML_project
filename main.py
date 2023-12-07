9c8a16080e957ad67740b4322e46d27945d8ff0a
Albert <a.iktisanov@yandex.ru>
2023-12-07 18:00:51 +0300

    VI KA

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
     primary = Primary_Data_Processing()
     machine = Machine_Learning()
     score = Scores()
-    data = primary.None_Processing() 
-    type = machine.lin_svm(data)
-    final_score = score.f1_macro(type)
+    X_train, X_test, y_train, y_test = primary.None_Processing()
+    prediction = machine.lin_svm(X_train, X_test, y_train)
+    final_score = score.accuracy_score(prediction, y_test)
     print('Наконец-то у меня спустя 5 часов дебага получился ответ:', final_score)
 
 if __name__ == '__main__':
