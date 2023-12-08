bf9751841ea461b0e3bcad7088497934ccd6882c
Albert <a.iktisanov@yandex.ru>
2023-12-09 00:59:11 +0300

    finally

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
     machine = Machine_Learning()
     score = Scores()
     X_train, X_test, y_train, y_test = primary.None_Processing()
-    prediction = machine.lin_svm(X_train, X_test, y_train)
-    final_score = score.accuracy_score(prediction, y_test)
+    prediction = machine.decision_tree(X_train, X_test, y_train)
+    final_score = score.f1_weighted(prediction, y_test)
     print('Наконец-то у меня спустя 5 часов дебага получился ответ:', final_score)
 
 if __name__ == '__main__':
